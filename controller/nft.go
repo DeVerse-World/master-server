976308c62a6ba05a4bee177932b455a236d52bf8
lth08091998 <lth08091998@gmail.com>
2022-04-16 22:32:43 +0800

    Mint nft api

--- controller/nft.go
+++ controller/nft.go
@@ -1,4 +1,19 @@
 package controller
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/hyperjiang/gin-skeleton/manager/schema"
+)
+
 type NftController struct{}
 
+func (ctrl *NftController) CreateMintNftLink(c *gin.Context) {
+	var req schema.CreateMinkLink
+	c.BindJSON(&req)
+
+	c.JSON(http.StatusOK, gin.H{"mint_nft_url": os.Getenv("UI_HOST") + "/marketplace/create-item?fileUri=" + req.IpfsHash})
+}
