eb707f06d30e9464b7f3f527c121f08e38d2cf3c
lth08091998 <lth08091998@gmail.com>
2022-06-10 20:58:02 +0700

    Change schema prevent duplicate join

--- sql/20220604135202-event_participants.sql
+++ sql/20220604135202-event_participants.sql
@@ -5,6 +5,7 @@ CREATE TABLE `event_participants` (
     `updated_at` timestamp NULL DEFAULT NULL,
     `created_at` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
+    UNIQUE KEY `event_participant_uniq` (`wallet_id`, `event_id`),
     `wallet_id` int unsigned DEFAULT NULL,
     CONSTRAINT `fk_event_participants_wallet` FOREIGN KEY (`wallet_id`) REFERENCES `wallets` (`id`),
     `event_id` int unsigned DEFAULT NULL,
